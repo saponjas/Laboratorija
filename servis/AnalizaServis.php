a6ca60acdb8335728cac3b33fa4519b24cad2930
saponjas <saponjasnezana96@gmail.com>
2021-12-16 20:07:29 +0100

    Kreiran broker i servisi za rad sa analizom i gradom

---
+++ servis/AnalizaServis.php
@@ -0,0 +1,21 @@
+<?php
+class AnalizaServis{
+    private $broker;
+
+    public function __construct($b){
+        $this->broker=$b;
+    }
+
+    public function vratiSve(){
+        return $this->broker->ucitajKolekciju('select * from analiza');
+    }
+
+    public function obrisi($id){
+        $this->broker->izmeni("delete from analiza where id=".$id);
+    }
+    public function kreiraj($analiza){
+        $this->broker->izmeni("insert into analiza (naziv) values ('".$analiza['naziv']."')");
+    }
+}
+
+?>

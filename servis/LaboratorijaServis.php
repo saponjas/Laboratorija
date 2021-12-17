6b26081f31bf79658de68d1bbe71032952d87df9
saponjas <saponjasnezana96@gmail.com>
2021-12-17 21:13:37 +0100

    Kreirana stranica za rad laboratorijama

--- servis/LaboratorijaServis.php
+++ servis/LaboratorijaServis.php
@@ -9,15 +9,9 @@ class LaboratorijaServis{
     public function vratiSve(){
         return $this->broker->ucitajKolekciju("select l.*, g.naziv as 'naziv_grada' from laboratorija l inner join grad g on (l.grad=g.id)");
     }
-    public function vratiJednu($id){
-        $res=$this->broker->ucitajKolekciju("select l.*, g.naziv as 'naziv_grada' from laboratorija l inner join grad g on (l.grad-g.id) where l.id=".$id);
-        if(count($res)==0){
-            throw new Exception('Laboratorija ne postoji');
-        }
-        $analize="select la.*, a.naziv from laboratorija_analiza la inner join analiza a where la.laboratorija=".$id;
-        $lab=$res[0];
-        $lab['analize']=$analize;
-        return $lab;
+    public function vratiSveAnalize($id){
+       return $this->broker->ucitajKolekciju("select la.*, a.naziv from laboratorija_analiza la inner join analiza a on (la.analiza=a.id) where la.laboratorija=".$id);
+
     }
     public function obrisi($id){
         $this->broker->izmeni("delete from laboratorija where id=".$id);
